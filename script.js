348da19bfe1b27f55a3a824ebc61158e08c644a3
Rich Capuy <rccapuy@gmail.com>
2020-03-28 09:41:40 -0400

    button redirection

---
+++ script.js
@@ -0,0 +1,13 @@
+// Create variables to DOM
+
+// Build Questions variable
+// Start Button function
+$(document).ready(function(){
+	var url = "https://google.com"
+	$("#start").on("click",function(){
+		$(location).attr("href", url);
+	})
+});
+// Build Timer to start
+// Add to Highscores
+
